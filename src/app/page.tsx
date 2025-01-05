5e6d802516b7ba9dd6ac4596ce8de4bc9c4eaf82
Hassan Ahmed Khan <161369304+HassanAhmedKhan31@users.noreply.github.com>
2025-01-05 18:28:34 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,13 +28,13 @@ export default function Home() {
 
       <section className="Home">
         <div className="content">
-          <h4>Hello, it's me</h4>
+          <h4>Hello, it&apos;s me</h4>
           <h1><span>Hassan Ahmed Khan</span> <br/> frontend developer</h1>
              <Image src={b} alt="profile" width={150} height={160} className="profile-picture absolute right-[200px] top-[150px] rounded-[100%]"  />
           <p>Lorem ipsum dolor sit amet consectetur <br/>adipisicing elit. Nihil sit corporis expedita!</p>
 
           <div className="button">
-            <a href="#" className="btn1">let's talk</a>
+            <a href="#" className="btn1">let&apos;s talk</a>
             <a href="#" className="btn2">portfolio</a>
           </div>
         </div>
